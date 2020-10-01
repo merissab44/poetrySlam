3bc6604add3e15d5cdb37c3449d4fca7ecee1429
Merissa Bridgeman <merissa.bridgeman98@gmail.com>
2020-09-30 23:27:24 -0700

    Update poem.py
    prints the poem lines backwards along with the line number

--- poem.py
+++ poem.py
@@ -4,3 +4,14 @@ def get_file_lines(filename):
     infile =  open(poem_file, "r").readlines() # read
     infile.close()
     return infile
+
+"""prints the line number and lines backwards"""
+def lines_printed_backwards(lines_list):
+    lines_list = open('/Users/merissabridgeman/dev/courses/CS 1.0/poetry/poem.txt').readlines()
+    num_lines = len(lines_list) # gets the length of the list
+    lines_list = lines_list[:: -1] # reverses the list
+
+    #for loop for the range of lines in the poem list
+    for line in range(len(lines_list)):
+        reverse = str(num_lines - line) + " " + lines_list[line] #beginning reverses the line numbers
+        print (reverse)
