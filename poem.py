96d6132439df3cf7b9a6603ac9be8c92e651b8f2
Merissa Bridgeman <merissa.bridgeman98@gmail.com>
2020-09-30 23:24:31 -0700

    Update poem.py
    returned file lines

--- poem.py
+++ poem.py
@@ -1,1 +1,6 @@
-create
+"""returns the lines inside the txt file"""
+def get_file_lines(filename):
+    poem_file = "/Users/merissabridgeman/dev/courses/CS 1.0/poetry/poem.txt" # opens file
+    infile =  open(poem_file, "r").readlines() # read
+    infile.close()
+    return infile
